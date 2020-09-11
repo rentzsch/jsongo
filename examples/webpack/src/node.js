18a3a14c4be21f53666a630ff837ec104fa6a85c
Alex N <alexnezhynsky@gmail.com>
2020-09-10 23:36:02 -0600

    Update node example to db.save() manually

--- examples/webpack/src/node.js
+++ examples/webpack/src/node.js
@@ -3,6 +3,7 @@ import { fsDB } from "jsongo";
 const db = fsDB("./db");
 db.users.deleteMany({}); // truncate
 db.users.insertMany([{ name: "guest" }, { name: "anonymous" }]);
+db.save(); // write to fs
 
 const users = db.users.find({});
 for (const user of users) {

3d975550b75120fd3ae29ab124d7105f5b32bb35
Alex N <alexnezhynsky@gmail.com>
2020-09-10 22:27:17 -0600

    Export proxy types so cons can annotate

--- lib/fs/jsongo.fs.ts
+++ lib/fs/jsongo.fs.ts
@@ -1,7 +1,9 @@
 import { JsongoFSDB } from "./JsongoFSDB";
 import { JsongoFSCollection } from "./JsongoFSCollection";
-import { createDBProxy } from "../shared";
+import { createDBProxy, DBProxy } from "../shared";
 import nodeFS from "fs";
 
-export const fsDB = (dirPath: string, fs?: typeof nodeFS) =>
-  createDBProxy<JsongoFSDB, JsongoFSCollection>(new JsongoFSDB(dirPath, fs));
+export type FSDBProxy = DBProxy<JsongoFSDB, JsongoFSCollection>;
+
+export const fsDB = (dirPath: string, fs?: typeof nodeFS): FSDBProxy =>
+  createDBProxy(new JsongoFSDB(dirPath, fs));
--- lib/mem/jsongo.mem.ts
+++ lib/mem/jsongo.mem.ts
@@ -1,6 +1,7 @@
 import { JsongoMemDB } from "./JsongoMemDB";
 import { JsongoMemCollection } from "./JsongoMemCollection";
-import { createDBProxy } from "../shared";
+import { createDBProxy, DBProxy } from "../shared";
 
-export const memDB = () =>
-  createDBProxy<JsongoMemDB, JsongoMemCollection>(new JsongoMemDB());
+export type MemDBProxy = DBProxy<JsongoMemDB, JsongoMemCollection>;
+
+export const memDB = (): MemDBProxy => createDBProxy(new JsongoMemDB());
